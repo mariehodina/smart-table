df40738761a7741b47dc860e7ebe28350a8059db
Masha Khodina <marie.hodina@yandex.by>
2026-02-07 17:29:19 +0300

    4 шаг выполнен

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,7 +3,10 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
-
+const comparator = createComparison(
+        [rules.skipEmptyTargetValues], 
+        [rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)]
+    );
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
         return data;
--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import { initTable } from "./components/table.js";
 import { initPagination } from "./components/pagination.js";
 import { initSorting } from "./components/sorting.js";
 import { initFiltering } from "./components/filtering.js";
+import { initSearching } from "./components/searching.js";
 // Исходные данные используемые в render()
 const { data, ...indexes } = initData(sourceData);
 
@@ -37,8 +38,9 @@ function render(action) {
   let state = collectState(); // состояние полей из таблицы
   let result = [...data]; // копируем для последующего изменения
   // @todo: использование
-  result = applyPagination(result, state, action);
-  result = applyPagination(result, state, action);
+  result = applySearching(result, state, action);
+  result = applyFiltering(result, state, action);
+  result = applySorting(result, state, action);
   result = applyPagination(result, state, action);
   sampleTable.render(result);
 }
@@ -47,7 +49,7 @@ const sampleTable = initTable(
   {
     tableTemplate: "table",
     rowTemplate: "row",
-    before: ["header", "filter"],
+    before: ['search', 'header', 'filter'],
     after: ["pagination"],
   },
   render,
@@ -72,6 +74,7 @@ const applySorting = initSorting([
 const applyFiltering = initFiltering(sampleTable.filter.elements, {
   searchBySeller: indexes.sellers,
 });
+const applySearching = initSearching('search');
 const appRoot = document.querySelector("#app");
 appRoot.appendChild(sampleTable.container);
 
